cd0bc821598477058bf22e7560617c845229c15b
David Slutsky <d6000d@gmail.com>
2021-04-04 22:25:26 +0300

    Initial commit.

---
+++ YouTubeIL/EntryPoint.cs
@@ -0,0 +1,65 @@
+﻿extern alias jb;
+
+using System;
+using System.Linq;
+using System.Net;
+using BepInEx;
+using HarmonyLib;
+using UnhollowerBaseLib;
+
+namespace YouTubeIL.BasePlugin
+{
+    [BepInPlugin(Id)]
+    [BepInProcess("Among Us.exe")]
+    [jb::JetBrains.Annotations.UsedImplicitly]
+    public class EntryPoint : BepInEx.IL2CPP.BasePlugin
+    {
+        [jb::JetBrains.Annotations.UsedImplicitly]
+        public const string Id = "il.r0den.YouTubeIL";
+        
+        private Harmony Harmony { get; } = new(Id);
+        
+        public override void Load()
+        {
+            Log.LogInfo("YouTubeIL plugin loading...");
+            Harmony.PatchAll();
+            
+            AddCustomServers();
+        }
+
+        private void AddCustomServers()
+        {
+            const string name = "YouTube IL";
+            // ReSharper disable StringLiteralTypo
+            var ip = "youtubeil.amongus.ainedevs.com";
+            // ReSharper restore StringLiteralTypo
+            if (Uri.CheckHostName(ip) == UriHostNameType.Dns)
+            {
+                Log.LogMessage("Server address is a hostname, resolving " + ip + "...");
+                try {
+                    foreach (var address in Dns.GetHostAddresses(ip))
+                    {
+                        if (address.AddressFamily != System.Net.Sockets.AddressFamily.InterNetwork) continue;
+                        ip = address.ToString();
+                        Log.LogMessage("Hostname resolved to " + ip);
+                        break;
+                    }
+                } catch {
+                    Log.LogMessage("Hostname could not be resolved");
+                }
+            }
+            
+            const ushort port = 22003;
+            
+            var defaultRegions = ServerManager.DefaultRegions.ToList();
+            Il2CppReferenceArray<ServerInfo> serverInfo = new ServerInfo[] { 
+                new(name, ip, port)
+            };
+
+            defaultRegions.Insert(0,
+                new StaticRegionInfo(name, StringNames.NoTranslation, ip, serverInfo).Cast<IRegionInfo>());
+
+            ServerManager.DefaultRegions = defaultRegions.ToArray();
+        }
+    }
+}
